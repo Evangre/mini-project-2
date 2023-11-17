4d8b7fa949d5dfb3ced33d11dc9e0dc3cb8a75b3
Evan Greene <Evan3greene@Gmail.com>
2023-11-16 19:44:55 -0500

    Refactor components to use custom hooks and shared FormField component

--- client/src/LoginComponent.js
+++ client/src/LoginComponent.js
@@ -1,60 +1,25 @@
+// LoginComponent.js
 import React, { useState } from "react";
+import { useFormInput } from "./useFormInput";
+import FormField from "./FormField";
 
-function LoginComponent() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+const LoginComponent = () => {
+  const username = useFormInput("");
+  const password = useFormInput("");
   const [submitting, setSubmitting] = useState(false);
   const [message, setMessage] = useState("");
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     setSubmitting(true);
-    setMessage("");
-
-    try {
-      const response = await fetch("/api/users/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ name: username, password }),
-      });
-
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-
-      const result = await response.json();
-      setMessage(result.message);
-    } catch (error) {
-      setMessage(error.message);
-    } finally {
-      setSubmitting(false);
-    }
   };
 
   return (
     <div>
       <h1>Login</h1>
       <form onSubmit={handleSubmit}>
-        <label>
-          Username:
-          <input
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            disabled={submitting}
-          />
-        </label>
-        <label>
-          Password:
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            disabled={submitting}
-          />
-        </label>
+        <FormField label="Username" type="text" {...username} />
+        <FormField label="Password" type="password" {...password} />
         <button type="submit" disabled={submitting}>
           Login
         </button>
@@ -62,6 +27,6 @@ function LoginComponent() {
       {message && <p>{message}</p>}
     </div>
   );
-}
+};
 
 export default LoginComponent;
--- client/src/RegisterComponent.js
+++ client/src/RegisterComponent.js
@@ -1,60 +1,25 @@
+// RegisterComponent.js
 import React, { useState } from "react";
+import { useFormInput } from "./useFormInput";
+import FormField from "./FormField";
 
-function RegisterComponent() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+const RegisterComponent = () => {
+  const username = useFormInput("");
+  const password = useFormInput("");
   const [submitting, setSubmitting] = useState(false);
   const [message, setMessage] = useState("");
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     setSubmitting(true);
-    setMessage("");
-
-    try {
-      const response = await fetch("/api/users/register", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ name: username, password }),
-      });
-
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-
-      const result = await response.json();
-      setMessage(result.message);
-    } catch (error) {
-      setMessage(error.message);
-    } finally {
-      setSubmitting(false);
-    }
   };
 
   return (
     <div>
       <h1>Register</h1>
       <form onSubmit={handleSubmit}>
-        <label>
-          Username:
-          <input
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            disabled={submitting}
-          />
-        </label>
-        <label>
-          Password:
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            disabled={submitting}
-          />
-        </label>
+        <FormField label="Username" type="text" {...username} />
+        <FormField label="Password" type="password" {...password} />
         <button type="submit" disabled={submitting}>
           Register
         </button>
@@ -62,6 +27,6 @@ function RegisterComponent() {
       {message && <p>{message}</p>}
     </div>
   );
-}
+};
 
 export default RegisterComponent;
